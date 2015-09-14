daf5c3d08b9036200e2f01d4158168a4244fa93b
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-09-14 11:32:54 -0300

    Chapter 7 accomplished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,5 +24,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
 end
