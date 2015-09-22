5db1ff7598b81fa707dc45443de3d8b8e2d4869c
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-09-22 11:06:58 -0300

    Remember token and digest - Model and database

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,19 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def remember
+      self remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
   end
 end
