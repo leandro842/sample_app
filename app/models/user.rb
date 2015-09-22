dc08e9696b8d7e23a05f8f786d1b1e607cfcc90c
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-09-22 16:19:12 -0300

    cookies implementation in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -18,14 +20,20 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
 
-    def User.new_token
-      SecureRandom.urlsafe_base64
-    end
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-    def remember
-      self remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
-    end
+  # Remember de user in the database for use in persistent sessions.
+  def remember
+    self remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest) == (remember_token)
   end
 end
