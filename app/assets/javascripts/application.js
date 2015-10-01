62507952f269712a131e2945e8f7b92368488fad
Leandro <ldesouzarodrigues@yahoo.com.br>
2015-10-01 10:08:04 -0300

    application.js apdated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
